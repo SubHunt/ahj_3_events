5df9cd4c4853c63f1d769015afa488f6056eaa61
SubHunt <phaeton.kz@gmail.com>
2024-04-08 16:52:05 +0300

    web.yml

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ahj_3_events');
-    m.set('version', '1.0.0');
-    return m;
-  }
-  
-  export default {
-    info
-  };
 import './css/style.css';
 
 import './js/app';
